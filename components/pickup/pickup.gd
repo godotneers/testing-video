b72cddf4a94dbece706b83b61e270a4d9ab3ef0a
Jan Thomä <janthomae@janthomae.de>
2025-07-30 08:58:50 +0200

    feat: add more tests

--- components/pickup/pickup.gd
+++ components/pickup/pickup.gd
@@ -5,15 +5,11 @@ extends Node3D
 
 
 ## Called when the pickup is dropped.
-## @param global_pos: The global position where the pickup should move to.
-func drop(global_pos: Vector3) -> void:
-	global_position = global_pos
+func drop() -> void:
 	_drop()
 
 ## Called when the pickup is picked up.
-## @param global_pos: The global position where the pickup should move to.
-func pick_up(global_pos: Vector3) -> void:
-	global_position = global_pos
+func pick_up() -> void:
 	_pick_up()
 
 ## Override this function in derived classes to implement pickup-specific drop behavior.
--- components/player_pawn/player_pawn.gd
+++ components/player_pawn/player_pawn.gd
@@ -9,7 +9,15 @@ signal pickup_detected(pickup: Pickup)
 ## @param pickup: The pickup that was lost
 signal pickup_lost(pickup: Pickup)
 
+## The currently held pickup
+var held_pickup: Pickup:
+	get:
+		return _held_pickup
+
 @onready var _pickup_detection_area: Area3D = Assure.exists(%PickupDetectionArea as Area3D)
+@onready var _hand_attachment: BoneAttachment3D = Assure.exists(%HandAttachment as BoneAttachment3D)
+
+var _held_pickup: Pickup = null
 
 func _ready() -> void:
 	_pickup_detection_area.area_entered.connect(_on_pickup_detection_area_entered)
@@ -28,3 +36,40 @@ func _on_pickup_detection_area_exited(area: Area3D) -> void:
 	var potential_pickup:Node = area.owner
 	if potential_pickup is Pickup:
 		pickup_lost.emit(potential_pickup)
+
+## Picks up the specified pickup
+## If the player is already holding a pickup, the current pickup will be dropped first
+## @param pickup: The pickup to pick up
+func pick_up(pickup: Pickup) -> void:
+	if not is_instance_valid(pickup):
+		return
+
+	# Drop any currently held pickup
+	if is_instance_valid(_held_pickup):
+		drop()
+
+	# Set the pickup's position to be at the hand attachment
+	_held_pickup = pickup
+	pickup.reparent(_hand_attachment)
+	pickup.position = Vector3.ZERO
+
+	# Call the pickup's pick_up method, so it can do additional setup
+	pickup.pick_up()
+
+## Drops the currently held pickup
+## Does nothing if no pickup is currently held
+func drop() -> void:
+	if not is_instance_valid(_held_pickup):
+		return
+	
+	# Reparent the pickup to the player's parent
+	_held_pickup.reparent(get_parent())
+
+	# Set the pickup's global position to be the player's global position
+	_held_pickup.global_position = global_position
+
+	# Call the pickup's drop method, so it can do additional cleanup
+	_held_pickup.drop()
+
+	#  And we no longer hold a pickup
+	_held_pickup = null
